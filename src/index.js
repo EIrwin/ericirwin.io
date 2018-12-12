9d537bcdd1d2c1fbb3c05e11721447c149d874d9
Eric Irwin <Eric.Irwin1124@gmail.com>
2018-12-11 20:52:32 -0700

    Fix bootstrap import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+// $FlowFixMe
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './routes';
 
 function AppWrapper() {
