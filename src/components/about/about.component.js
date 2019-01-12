f2cb2f620dab9253a316f9d3e080df8f7db042e5
Eric Irwin <Eric.Irwin1124@gmail.com>
2019-01-12 16:21:38 -0700

    About iteration

--- src/components/about/about.component.js
+++ src/components/about/about.component.js
@@ -3,7 +3,17 @@ import React from 'react';
 import Interests from './interests.component';
 import './about.css';
 
+
+const RYANNS_BIRTHDAY =  new Date(2014, 6, 24);
+const LOGANS_BIRTHDAY =  new Date(2017, 11, 17);
 export default function About() {
+
+  const getDurationYears = (date: Date): number => {
+    const CONVERSION_MS=31536000000;
+    const current = new Date();
+    return Math.floor((current - date)/CONVERSION_MS);
+  };
+
   return (
     <section id="about">
       <div className="container">
@@ -18,10 +28,25 @@ export default function About() {
           <div className="col-sm-8">
             <h2>About Me</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus pretium rutrum vulputate. Sed auctor tortor nec nibh
-              ultricies cursus in vitae dui. Donec vel ligula vitae quam
-              ultrices porttitor vitae a ligula.
+              Although I was born and raised in Arizona, I pursued my undergraduate degree at
+              <b> The Citadel Military College of South Carolina </b>
+              where I graduated <b>Cum Laude</b> with a <b>B.S. in Computer Science </b>
+              and a minor in <b>Management Information Systems </b>.
+            </p>
+            <p>
+              Upon graduating, I returned to Arizona to begin my career in <b>Software Engineering </b>
+              as well as earn a <b>Masters of Science Engineering in Software Engineering</b> from
+              <b> Arizona Status University</b>.
+            </p>
+            <p>
+              In October 2016, I started my current role as <b>Senior Software Engineer/Site Reliability Engineer</b> at <b>CommercialTribe</b> and relocated to beautiful <b>Denver, Colorado</b>.
+            </p>
+            <p>
+              I have been extremely fortunate in my career to have been given the opportunities to work with so many
+              talented engineers and innovated teams that have helped mold me into the <b>Engineer</b> and <b>Leader</b> I am today.
+            </p>
+            <p>
+              I have a beautiful wife, <b>Jaymie</b> and two beautiful daughters, <b>Ryann ({getDurationYears(RYANNS_BIRTHDAY)})</b> and <b>Logan ({getDurationYears(LOGANS_BIRTHDAY)})</b> who mean the world to me.
             </p>
             <div className="row">
               <div className="col">
