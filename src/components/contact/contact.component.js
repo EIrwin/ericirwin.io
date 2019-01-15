3503e23c10daf0b9aac7d3e924358c14743613a5
Eric Irwin <Eric.Irwin1124@gmail.com>
2019-01-15 05:41:01 -0700

    Add SPARKPOST_KEY env var

--- src/components/contact/contact.component.js
+++ src/components/contact/contact.component.js
@@ -40,7 +40,6 @@ class Contact extends React.Component<Props, State> {
     if (!error) {
       // TODO submit contact form
     }
-
     const sparky = new SparkPost(Config.SparkPostKey);
 
     sparky.transmissions
