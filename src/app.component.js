dbf16c783f579b741afe5651575de5d51068f646
Eric Irwin <Eric.Irwin1124@gmail.com>
2019-01-16 20:34:37 -0700

    Dont use helmet

--- src/app.component.js
+++ src/app.component.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import ReactGA from 'react-ga';
 import { About } from './components/about';
 import { Contact } from './components/contact';
@@ -21,28 +20,6 @@ const AppComponent = () => {
   initializeReactGA();
   return (
     <div>
-      <Helmet>
-        <title>Eric Irwin</title>
-        <meta
-          name="description"
-          content="Hi, I'm Eric Irwin, a Sr. Software Engineer!"
-        />
-        <meta name="theme-color" content="##11ABB0" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="noindex, nofollow" />
-        <meta property="og:site_name" content="Eric Irwin" />
-        <meta property="og:title" content="Eric Irwin" />
-        <meta
-          property="og:description"
-          content="Hi, I'm Eric Irwin, a Sr. Software Engineer!"
-        />
-        <meta property="og:url" content="https://ericirwin.io" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://avatars0.githubusercontent.com/u/6673161?s=460&v=4"
-        />
-      </Helmet>
       <Navigation />
       <Header />
       <About />
